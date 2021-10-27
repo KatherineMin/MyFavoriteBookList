90a4c9927d85eb6228c8f72a7f0de6dc00e10b99
Katherine Min <msyeon1028@gmail.com>
2021-10-27 17:20:21 -0400

    in-between

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  // connect to mongodb
+  "URI" : "mongodb://localhost/comp229midterm"
+  // "URI" : "mongodb+srv://katherine_001:comp229midterm@cluster0.pb2sl.mongodb.net/comp229midterm?retryWrites=true&w=majority"
+};
